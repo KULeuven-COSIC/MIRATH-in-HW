55cb9ccd683d7ccb22b79302f7de95d888f062d9
Stelios Manasidis <63156068+Stelios443@users.noreply.github.com>
2026-04-23 21:48:30 +0300

    Add comments and licensing to permute_64.v
    Add header comments and licensing information to permute_64.v

--- src_rtl/permute_64.v
+++ src_rtl/permute_64.v
@@ -1,3 +1,24 @@
+/*
+ * permute_64.v
+ * ----------------------
+ * This file implements a combinational Keccak-f[1600] round function,
+  * including theta, rho, pi, chi, and iota, on a lane-major state.
+ *
+ * Copyright (c) 2026 KU Leuven - COSIC
+ * Author: Stelios Manasidis    
+ *        
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
 `include "clog2.v"
 `include "keccak_pkg_64.v"
 
