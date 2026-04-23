8382cbada33fb36e657e82b022c8d750c942487f
Stelios Manasidis <63156068+Stelios443@users.noreply.github.com>
2026-04-23 19:48:33 +0300

    Add comments and license to C_mem.sv
    Add header comments and licensing information to C_mem.sv

--- src_rtl/C_mem.sv
+++ src_rtl/C_mem.sv
@@ -1,3 +1,23 @@
+/*
+ * C_mem.sv
+ * ---------
+ * This file implements a RAM that stores the secret (witness) matrix C'
+ *
+ * Copyright (c) 2026 KU Leuven - COSIC
+ * Author: Stelios Manasidis    
+ *        
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+ 
 `include "math.vh"
 
 `default_nettype none
