9c7eff0e74b572caa78cc73d346a37d0049ead95
Stelios Manasidis <63156068+Stelios443@users.noreply.github.com>
2026-04-23 21:52:42 +0300

    Add comments and license to tb_sign_verify.sv
    Add header comments and licensing information to tb_sign_verify.sv

--- src_tb/tb_sign_verify.sv
+++ src_tb/tb_sign_verify.sv
@@ -1,3 +1,23 @@
+/*
+ * tb_sign_verify.sv
+ * -------------
+ * This is a testbench that runs a single sign & verify run.
+ *
+ * Copyright (c) 2026 KU Leuven - COSIC
+ * Author: Stelios Manasidis    
+ *        
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+ 
 `timescale 1ns / 1ps
 
 `include "mirath_hw_params.vh"
